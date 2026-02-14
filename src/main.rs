0afac73886c0f0e04865930093b9b41dba87bc72
sksizer <shane@sizerllc.com>
2026-02-13 22:51:59 -0600

    fsm stuff

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+enum AppErrorStates {
+    NoConfigFile,
+    InvalidConfigFile,
+    FailedToStart,
+}
+
+#[derive(Debug)]
+enum AppState {
+    New,
+    Initializing,
+    FirstRun,
+    Running,
+    Error(AppErrorStates),
+}
+
+
+#[derive(Debug)]
+enum AppEvent {
+    Start,
+    Stop,
+    Restart,
+}
+
+fn new() -> AppState {
+    AppState::New
+}
+
+fn startApp(_state: AppState::New) -> AppState {
+    AppState::Initializing
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    let mut app_state = new();
+    println!("App State: {:?}", app_state);
+
+    app_state = startApp(app_state);
+    println!("App State: {:?}", app_state);
+}
